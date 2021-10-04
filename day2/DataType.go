a2382ea6ea30df1eec0b5a844ee6c5090e8f7ee8
rainforest-rainxuan <rainforestxuan@163.com>
2021-10-04 23:17:01 +0800

    这是第二次学习go语言的笔记代码

--- day2/DataType.go
+++ day2/DataType.go
@@ -2,12 +2,18 @@ package main
 
 import "fmt"
 
+type MyInt64 int64
+
 func test1() {
 	var a int = 1
 	var b int64
 	//b = a
+	var c MyInt64
+	//c = a	 //cannot use a (type int) as type MyInt64 in assignment
+	c = MyInt64(a)
 	b = int64(a)
 	fmt.Println(b)
+	fmt.Println(c)
 }
 
 func main() {
